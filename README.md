28ccdb2f62fd3657297203d912ff42235080534c
fepc18 <fepc18@gmail.com>
2023-04-12 14:14:18 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Teslo API
+1. Clonar proyecto
+2. ```Clonar el archivo ```.env.template``` y renombrarlo a ```.env```
+3. Cambiar las variables de entorno
+
+5. Levantar la base de datos
+
+```
+docker-compose up
+```
+
+
+6. Levantar: yarn start:dev
+//https://github.com/Klerith/nest-teslo-shop/tree/fin-seccion-10
+
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,51 @@
+import {
+  IsString,
+  MinLength,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsArray,
+  IsIn,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men','women','kid','unisex'])
+  gender: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  
+  tags: string[];
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  images: string[];
+
+}
